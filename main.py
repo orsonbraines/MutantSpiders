5d9ef1d943356a9253de29efcf3a2a0f3e8cff40
Mark <markhskinner@gmail.com>
2021-07-10 21:57:20 -0600

    port to JS and added some brainstorms

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class Spider:
 
 	def draw(self, surface):
 		self.draw_head(surface)
-		#pg.draw.ellipse(surface, (0,0,0), (-12 + self.x, 3 + self.y, 24, 40))
 		self.draw_body(surface)
 		self.draw_legs(surface)
 
