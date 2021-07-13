2650dff3e42e4077b3e8b735223d88aa75c3e314
Orson Baines <obraines@gmail.com>
2021-07-12 18:36:27 -0600

    add transforms

--- spider.js
+++ spider.js
@@ -2,12 +2,13 @@ class Spider {
 	constructor() {
 		this.x = 100;
 		this.y = 100;
+		this.theta = 0;
 		this.head_r = 10;
 		this.body_rx = 12;
 		this.body_ry = 20;
 		this.leg_angles = [
-			[-70, -10, 10, 40, 125, 155, 185, 210],
-			[-35, -5, 25, 50, 140, 170, 190, 250]
+			[110, 170, 190, 220, 305, 335, 5, 30],
+			[145, 175, 205, 230, 320, 350, 10, 70]
 		];
 		this.frame = 0;
 		for(let i = 0; i < 2; ++i) {
@@ -19,70 +20,44 @@ class Spider {
 	}
 
 	draw(ctx) {
-		this.draw_legs(ctx);
-		this.draw_body(ctx);
-		this.draw_head(ctx);
+		const t = new Transform([this.x, this.y], this.theta);
+		this.draw_legs(ctx, t);
+		this.draw_body(ctx, t);
+		this.draw_head(ctx, t);
 	}
 
-	draw_legs(ctx) {
+	draw_legs(ctx, t) {
 		for(let i = 0; i < 8; ++i) {
 			ctx.beginPath();
-			ctx.moveTo(this.x + this.head_r * Math.cos(this.leg_angles[this.frame][i]),
-				this.y + this.head_r * Math.sin(this.leg_angles[this.frame][i]));
-			ctx.lineTo(this.x + (this.head_r + this.leg_length) * Math.cos(this.leg_angles[this.frame][i]),
-				this.y + (this.head_r + this.leg_length) * Math.sin(this.leg_angles[this.frame][i]));
+			const p1 = t.apply([this.head_r * Math.cos(this.leg_angles[this.frame][i]),
+				this.head_r * Math.sin(this.leg_angles[this.frame][i])]);
+			const p2 = t.apply([(this.head_r + this.leg_length) * Math.cos(this.leg_angles[this.frame][i]),
+				(this.head_r + this.leg_length) * Math.sin(this.leg_angles[this.frame][i])]);
+			ctx.moveTo(p1[0], p1[1]);
+			ctx.lineTo(p2[0], p2[1]);
 			ctx.stroke();
 		}
 	}
 
-	draw_head(ctx) {
+	draw_head(ctx, t) {
 		ctx.beginPath();
-		ctx.ellipse(this.x, this.y, this.head_r, this.head_r, 0, 0, Math.PI * 2);
+		const center = t.apply([0,0]);
+		const r = t.scale * this.head_r;
+		ctx.ellipse(center[0], center[1], r, r, 0, 0, Math.PI * 2);
 		ctx.fill();
 	}
 
-	draw_body(ctx) {
+	draw_body(ctx, t) {
 		ctx.beginPath();
-		ctx.ellipse(this.x, this.y + this.head_r + 15, this.body_rx, this.body_ry, 0, 0, Math.PI * 2);
+		const center = t.apply([0,-this.head_r - this.body_ry/2]);
+		const rx = t.scale * this.body_rx;
+		const ry = t.scale * this.body_ry;
+		ctx.ellipse(center[0], center[1], rx, ry, -this.theta, 0, Math.PI * 2);
 		ctx.fill();
 	}
+
 	tick(){
 		this.frame = 1 - this.frame;
+		this.theta += 0.05;
 	}
 }
-
-// class Spider:
-// 	def __init__(self):
-// 		self.x = 100
-// 		self.y = 100
-// 		self.head_r = 10
-// 		self.body_rx = 12
-// 		self.body_ry = 20
-// 		self.leg_angles = [
-// 			[-70, -10, 10, 40, 125, 155, 185, 210],
-// 			[-35, -5, 25, 50, 140, 170, 190, 250]
-// 		]
-// 		self.frame = 0
-// 		for i in range(2):
-// 			for j in range(8):
-// 				self.leg_angles[i][j] = math.radians(self.leg_angles[i][j])
-// 		self.leg_length = 40
-//
-// 	def draw(self, surface):
-// 		self.draw_head(surface)
-// 		self.draw_body(surface)
-// 		self.draw_legs(surface)
-//
-// 	def draw_head(self, surface):
-// 		pg.draw.ellipse(surface, (0,0,0), (-self.head_r + self.x, -self.head_r + self.y, self.head_r * 2, self.head_r * 2))
-//
-// 	def draw_body(self, surface):
-// 		pg.draw.ellipse(surface, (0,0,0), (-self.body_rx + self.x, self.head_r - 9 + self.y, self.body_rx * 2, self.body_ry * 2))
-//
-// 	def draw_legs(self, surface):
-// 		for i in range(8):
-// 			pg.draw.line(surface, (0,0,0), (self.x + self.head_r * math.cos(self.leg_angles[self.frame][i]), self.y + self.head_r * math.sin(self.leg_angles[self.frame][i])),
-// 				(self.x + (self.head_r + self.leg_length) * math.cos(self.leg_angles[self.frame][i]), self.y + (self.head_r + self.leg_length) * math.sin(self.leg_angles[self.frame][i])), 2)
-//
-// 	def tick(self):
-// 		self.frame = 1 - self.frame
---
+++ transform.js
@@ -0,0 +1,19 @@
+class Transform {
+	constructor(pos, theta, flipY = true) {
+		this.xx = Math.cos(theta);
+		this.xy = -Math.sin(theta);
+		this.xt = pos[0];
+		this.yx = Math.sin(theta);
+		this.yy = Math.cos(theta);
+		if(flipY) {
+			this.yx *= -1;
+			this.yy *= -1;
+		}
+		this.yt = pos[0];
+		this.scale = 1;
+	}
+
+	apply(vec) {
+		return [vec[0] * this.xx + vec[1] * this.xy + this.xt, vec[0] * this.yx + vec[1] * this.yy + this.yt];
+	}
+}
