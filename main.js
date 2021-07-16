9ca1ddcc7640b55452e4a19c01844b06f4f27787
Orson Baines <obraines@gmail.com>
2021-07-15 22:04:43 -0600

    add spider moving + frame counter into spider.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function resizeCanvas(){
 
 function initGame(){
 	resizeCanvas();
-	frames = 0;
 	ctx.strokeStyle = "#000";
 	ctx.lineWidth = 2;
 	ctx.lineCap = 'round';
@@ -21,26 +20,12 @@ window.addEventListener('resize',e => {
 });
 
 function gameLoop(){
-	const delta = [mouse.x - spider.x, mouse.y - spider.y];
-	deltaR = Math.sqrt(delta[0] * delta[0] + delta[1] * delta[1]);
-	spider.theta = Math.atan2(-delta[1], delta[0]) - Math.PI / 2;
-	//console.log(delta[0],delta[1],spider.theta);
-	const v = 5;
-	spider.moving = (deltaR >= v)
-	if(spider.moving) {
-		spider.x += delta[0] * v / deltaR;
-		spider.y += delta[1] * v / deltaR;
-	}
-
 	drawGame();
-	frames++;
 	requestAnimationFrame(gameLoop);
 }
 function drawGame(){
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	if(frames % 10 == 0){
-		spider.tick();
-	}
+	spider.tick();
 	player.tick();
 	spider.draw(ctx);
 	player.draw(ctx);
