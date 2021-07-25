54d3ebdbc096b8696d50244cebd5b4a6a1df5974
Mark <markhskinner@gmail.com>
2021-07-25 14:50:33 -0600

    fixed the camera and the river. The camera may have fell in

--- main.js
+++ main.js
@@ -32,7 +32,13 @@ function tickAll(){
 function drawGame(){
 	ctx.resetTransform();
 	ctx.clearRect(0,0,canvas.width, canvas.height);
-	cameraTransform = viewMatrix(player.x, 1500,canvas.width, player.y, 1500.0 * canvas.height / canvas.width, canvas.height);
+	// cameraTransform = viewMatrix(player.x, 1500,canvas.width, player.y, 1500.0 * canvas.height / canvas.width, canvas.height);
+	const gameWidth = 1500;
+	const gameHeight = 1500 * (canvas.height / canvas.width);
+	const cameraPull = 1.0 / 9.2;
+	cameraTransform = viewMatrix(player.x + (mouse.x-canvas.width/2)*(gameWidth/canvas.width)*cameraPull,
+			gameWidth,canvas.width,
+			player.y + (mouse.y - canvas.height/2)*(gameHeight/canvas.height) * cameraPull, gameHeight, canvas.height);
 	cameraTransform.setToCtx(ctx);
 	ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 	ctx.drawImage(tree_5, 0, 0, canvas.width/9, canvas.height/6);
@@ -47,12 +53,12 @@ function drawGame(){
 	// ctx.drawImage(river_2, 300, 380, canvas.width/24, canvas.height/15);
 	// ctx.drawImage(river_2, 300, 390, canvas.width/24, canvas.height/15);
 	//ctx.drawImage(river_2, 300, 400, canvas.width/24, canvas.height/15);
-	var c = 100
-	for(i = 100; i<400; i++){
-		c = c +.3
+	var c = 100;
+	for(i = 0; i < gameHeight; ++i){
+		c = c + 0.3;
+		c = Math.random() * 1500;
 		ctx.drawImage(river_2, c, i, canvas.width/19, canvas.height/15);
 	}
-	var c = 100
 
 	spider.draw(ctx);
 	player.draw(ctx);
